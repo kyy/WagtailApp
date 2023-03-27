789ae93e2f454572502fd7b4f6c1defe77339526
xibolba <insider_2102@mail.ru>
2023-03-27 16:39:04 +0300

    signal: optimize_image_storage_by_resize_images_on_upload

--- home/wagtail_hooks.py
+++ home/wagtail_hooks.py
@@ -1,5 +1,5 @@
 import os
-from django.db.models.signals import  post_save
+from django.db.models.signals import post_save, pre_save
 from django.dispatch import receiver
 from wagtail.images import get_image_model
 
@@ -20,3 +20,23 @@ def resize_images_on_upload(sender, instance, created=False, **kwargs):
             instance.height = croped_image.height
             instance.file_size = os.path.getsize(original_file)
             instance.save()
+
+
+# @receiver(post_save, sender=get_image_model())
+# def resize_images_on_edit(sender, instance, **kwargs):
+#     if (instance.width and instance.height) > 2000:
+#         old = sender.objects.get(pk=instance.pk)
+#         print('old', old)
+#         if old.file.path != instance.file.path:
+#             croped_image = instance.get_rendition('max-2000x2000|jpegquality-80')
+#             croped_file = croped_image.file.path
+#             original_file = instance.file.path
+#             copy_o_f = original_file
+#             try:
+#                 os.rename(croped_file, original_file)
+#             except FileExistsError:
+#                 os.replace(croped_file, copy_o_f)
+#             instance.width = croped_image.width
+#             instance.height = croped_image.height
+#             instance.file_size = os.path.getsize(original_file)
+#             instance.save()
