e4794b4e1ced70615034e93f93012c21b6432777
kyy <insider_2012@mail.ru>
2023-03-27 18:39:56 +0300

    resize_images_on_edit signal

--- home/wagtail_hooks.py
+++ home/wagtail_hooks.py
@@ -11,32 +11,31 @@ def resize_images_on_upload(sender, instance, created=False, **kwargs):
             croped_image = instance.get_rendition('max-2000x2000|jpegquality-80')
             croped_file = croped_image.file.path
             original_file = instance.file.path
-            copy_o_f = original_file
             try:
                 os.rename(croped_file, original_file)
             except FileExistsError:
-                os.replace(croped_file, copy_o_f)
+                os.replace(croped_file, original_file)
             instance.width = croped_image.width
             instance.height = croped_image.height
             instance.file_size = os.path.getsize(original_file)
             instance.save()
+            print('upload image signal')
 
 
-# @receiver(post_save, sender=get_image_model())
-# def resize_images_on_edit(sender, instance, **kwargs):
-#     if (instance.width and instance.height) > 2000:
-#         old = sender.objects.get(pk=instance.pk)
-#         print('old', old)
-#         if old.file.path != instance.file.path:
-#             croped_image = instance.get_rendition('max-2000x2000|jpegquality-80')
-#             croped_file = croped_image.file.path
-#             original_file = instance.file.path
-#             copy_o_f = original_file
-#             try:
-#                 os.rename(croped_file, original_file)
-#             except FileExistsError:
-#                 os.replace(croped_file, copy_o_f)
-#             instance.width = croped_image.width
-#             instance.height = croped_image.height
-#             instance.file_size = os.path.getsize(original_file)
-#             instance.save()
+@receiver(post_save, sender=get_image_model())
+def resize_images_on_edit(sender, instance, **kwargs):
+    if (instance.width and instance.height) > 2000:
+        old = sender.objects.get(pk=instance.pk)
+        if old.file.path == instance.file.path:
+            croped_image = instance.get_rendition('max-2000x2000|jpegquality-80')
+            croped_file = croped_image.file.path
+            original_file = instance.file.path
+            try:
+                os.rename(croped_file, original_file)
+            except FileExistsError:
+                os.replace(croped_file, original_file)
+            instance.width = croped_image.width
+            instance.height = croped_image.height
+            instance.file_size = os.path.getsize(original_file)
+            instance.save()
+            print('edit image signal')
