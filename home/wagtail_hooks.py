2aeac7c6df7a10a3c167d9c8cd192b9cbba7f312
kyy <insider_2012@mail.ru>
2023-03-27 18:44:51 +0300

    resize_images_on_upload_or_edit 2-in-1

--- home/wagtail_hooks.py
+++ home/wagtail_hooks.py
@@ -5,28 +5,10 @@ from wagtail.images import get_image_model
 
 
 @receiver(post_save, sender=get_image_model())
-def resize_images_on_upload(sender, instance, created=False, **kwargs):
-    if (instance.width and instance.height) > 2000:
-        if created:
-            croped_image = instance.get_rendition('max-2000x2000|jpegquality-80')
-            croped_file = croped_image.file.path
-            original_file = instance.file.path
-            try:
-                os.rename(croped_file, original_file)
-            except FileExistsError:
-                os.replace(croped_file, original_file)
-            instance.width = croped_image.width
-            instance.height = croped_image.height
-            instance.file_size = os.path.getsize(original_file)
-            instance.save()
-            print('upload image signal')
-
-
-@receiver(post_save, sender=get_image_model())
-def resize_images_on_edit(sender, instance, **kwargs):
+def resize_images_on_upload_or_edit(sender, instance, created=False, **kwargs):
     if (instance.width and instance.height) > 2000:
         old = sender.objects.get(pk=instance.pk)
-        if old.file.path == instance.file.path:
+        if created or old.file.path == instance.file.path:
             croped_image = instance.get_rendition('max-2000x2000|jpegquality-80')
             croped_file = croped_image.file.path
             original_file = instance.file.path
@@ -38,4 +20,3 @@ def resize_images_on_edit(sender, instance, **kwargs):
             instance.height = croped_image.height
             instance.file_size = os.path.getsize(original_file)
             instance.save()
-            print('edit image signal')
