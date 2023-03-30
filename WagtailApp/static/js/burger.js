d0ffbf0dd0bab21064cb8823dd0035e59b5c2a87
xibolba <insider_2102@mail.ru>
2023-03-30 16:10:49 +0300

    burger menu

---
+++ WagtailApp/static/js/burger.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+    $('.header__burger').click(function(event) {
+        $('.header__burger,.header__menu').toggleClass('active');
+        $('body').toggleClass('lock');
+    });
+});
+
+// закрытие меню при клике на ссылку
+$(document).ready(function() {
+    $('.header__list').click(function(event) {
+        $('.header__burger,.header__menu').toggleClass('active');
+        $('body').toggleClass('lock');
+    });
+});
