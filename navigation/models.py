0da6a28832e29519bb0378faac5dc59e2a82ed38
kyy <insider_2012@mail.ru>
2023-04-21 21:26:13 +0300

    edit handler

--- navigation/models.py
+++ navigation/models.py
@@ -1,5 +1,5 @@
 import wagtail.models
-from wagtail.admin.panels import FieldPanel, HelpPanel
+from wagtail.admin.panels import FieldPanel, HelpPanel, TabbedInterface, ObjectList
 from wagtail.contrib.settings.models import BaseSiteSetting, register_setting
 from wagtail.fields import StreamField
 from .blocks import (
@@ -26,8 +26,8 @@ class NavigationMenuSetting(BaseSiteSetting):
         HelpPanel(
             content='<div class="help-block help-info"><svg class="icon icon-help icon" aria-hidden="true">'
                     '<use href="#icon-help"></use></svg><p>Категория меню не будет отображаться если страница не '
-                    'опубликована или</p><p> отсутствует отметка - <i>"Показывать в меню" </i>в параметрах страницы '
-                    'на вкладке: </p><p><i>"Продвижение"</i>-><i>"Для меню сайта"</i></p></div>',
+                    'опубликована или</p><p> отсутствует отметка - <i>«Показывать в меню» </i>в параметрах страницы '
+                    'на вкладке: </p><p><i>«Продвижение»</i> --> <i>«Для меню сайта»</i></p></div>',
             heading='Инструкция',
         ),
         FieldPanel(
@@ -35,13 +35,9 @@ class NavigationMenuSetting(BaseSiteSetting):
             heading='Конструктор категорий меню',
         ),
     ]
-
-    def get_url(self):
-        # site_id = NavigationMenuSetting.objects.only('site_id').prefetch_related('site').first().site_id
-        site_id = get_object_or_404(
-            NavigationMenuSetting.objects.only('site_id'),
-            site__id__in=wagtail.models.Site.objects.all()).site_id
-        return f'/{site_id}'
+    edit_handler = TabbedInterface([
+        ObjectList(panels, heading='«Меню-Бургер»'),
+    ])
 
     class Meta:
         verbose_name = "Меню навигации"
